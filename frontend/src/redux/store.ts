d1b5e1bb4758c4d8a88421e5b0341f7810a249b2
inFamousxD <audimankar@gmail.com>
2025-12-21 04:06:37 -0500

    Del Explorer Slice

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,13 +1,11 @@
 import {configureStore} from "@reduxjs/toolkit";
 import userSlice from "./slices/data/user";
 import mainDockSlice from "./slices/application/mainDock";
-import explorerSlice from "./slices/data/explorer";
 
 const store = configureStore({
     reducer: {
         user: userSlice.reducer,
         mainDock: mainDockSlice.reducer,
-        explorer: explorerSlice.reducer
     }
 });
 
